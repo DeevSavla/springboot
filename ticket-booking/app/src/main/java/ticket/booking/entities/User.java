706a8026a0bff787671d4f17f7d37e3f4e488144
Deev Savla <savla.deev@gmail.com>
2026-02-19 12:19:01 +0530

    add getter setter for user

--- ticket-booking/app/src/main/java/ticket/booking/entities/User.java
+++ ticket-booking/app/src/main/java/ticket/booking/entities/User.java
@@ -4,10 +4,63 @@ import java.util.List;
 
 public class User {
     private String name;
-    private String password;
     private String hashed_password;
     private List<Ticket> tickets_booked;
     private String user_id;
+
+    public User(String name, String hashed_password, List<Ticket> tickets_booked, String user_id) {
+        this.name = name;
+        this.hashed_password = hashed_password;
+        this.tickets_booked = tickets_booked;
+        this.user_id = user_id;
+    }
+
+    public User() {
+    }
+
+    // Getter Functions
+
+    public String getName() {
+        return name;
+    }
+
+    public String getHashedPassword() {
+        return hashed_password;
+    }
+
+    public List<Ticket> getTicketsBooked() {
+        return tickets_booked;
+    }
+
+    public String getUserId() {
+        return user_id;
+    }
+
+    // Print Tickets
+
+    public void printTickets() {
+        for (int i = 0; i < tickets_booked.size(); i++) {
+            System.out.println(tickets_booked.get(i).getTicketsInfo());
+        }
+    }
+
+    // Setter functions
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setHashedPassword(String hashed_password) {
+        this.hashed_password = hashed_password;
+    }
+
+    public void setTicketsBooked(List<Ticket> tickets_booked) {
+        this.tickets_booked = tickets_booked;
+    }
+
+    public void setUserId(String user_id) {
+        this.user_id = user_id;
+    }
 }
 
-//private so that no one access this variables directly, access using getters / setters.
+// private so that no one access this variables directly, access using getters / setters.
--- ticket-booking/app/src/main/java/ticket/booking/services/UserService.java
+++ ticket-booking/app/src/main/java/ticket/booking/services/UserService.java
@@ -3,6 +3,7 @@ package ticket.booking.services;
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
+import java.util.Optional;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -10,25 +11,44 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import ticket.booking.entities.User;
 
 public class UserService {
-    
+
     private User user;
 
     private List<User> userList;
 
-    private static final String USERS_PATH = "../localDB/users.json";
+    private static final String USERS_PATH = "app/src/main/java/ticket/booking/localDB/users.json";
 
-    //serialize - java object to json 
-    //deserialize - ulta
+    // serialize - java object to json
+    // deserialize - ulta
     private ObjectMapper objectMapper = new ObjectMapper();
 
-    //constructor
-    public UserService (User user) throws IOException {
-        this.user = user;
+    // constructor
+    public UserService(User user1) throws IOException {
+        this.user = user1;
 
         File users = new File(USERS_PATH);
 
-        userList = objectMapper.readValue(users,new TypeReference<List<User>>() {});
+        userList = objectMapper.readValue(users, new TypeReference<List<User>>() {
+        });
     }
 
-        
+    public boolean loginUser() {
+
+        //stream - stream api -> creates a new array after filtering it with a condition.
+        //function are first class they can be as argument, parameters and can be used.
+        //predicate is a boolean function that returns T/F based on condition in filter.
+        //Predicate<? super T> - this means T if any predicate has User or its parent class.
+        //stream() - Converts list into pipeline
+        //map() - Transforms elements & creates a new list
+        //filter() - Removes elements
+        //collect() - Converts stream back to list
+
+        // Optional<User> foundUser = userList.stream().filter(user1 => {
+        //     return user1.getName().equals(user.getName()) 
+        // })
+
+        //return foundUser.isPresent();
+
+        return true;
+    }
 }
